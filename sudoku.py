db626e803c63c519c781c2e4d088583a43c59551
Giuseppe Bellamacina <giuseppe.bellamacina@studium.unict.it>
2024-09-07 22:48:40 +0200

    Updated grid functionalities

--- sudoku.py
+++ sudoku.py
@@ -1,25 +1,39 @@
 import time
+import random
 from os import system, name
 
 class Grid():
-    def __init__(self, grid):
+    def __init__(self, grid, square_root_dim):
         self.grid = grid
-        self.size = len(grid)
+        self.square_root_dim = square_root_dim
+        self.size = square_root_dim**2
         self.zeros = []
         self.skippable_numbers = {}
         self.index = 0
         self.count = 0
+    
+    @classmethod
+    def from_grid(cls, grid):
+        square_root_dim = int(len(grid)**0.5)
+        return cls(grid, square_root_dim)
+    
+    @classmethod
+    def from_dim(cls, square_root_dim):
+        tmp = cls([], square_root_dim)
+        tmp.generate_grid()
+        return tmp
         
     def __str__(self):
+        space = lambda x: ' ' * (len(str(self.size)) - len(str(x)))
         s = ''
         for i in range(self.size):
             for j in range(self.size):
                 if (i, j) in self.zeros and self.grid[i][j] == 0:
-                    s += '\33[31m0\33[0m '
+                    s += space(0) +'\33[31m0\33[0m '
                 elif (i, j) in self.zeros and self.grid[i][j] != 0:
-                    s += '\33[32m' + str(self.grid[i][j]) + '\33[0m '
+                    s += space(self.grid[i][j]) +'\33[32m' + str(self.grid[i][j]) + '\33[0m '
                 else:
-                    s += str(self.grid[i][j]) + ' '
+                    s += space(self.grid[i][j]) + str(self.grid[i][j]) + ' '
             s += '\n'
         return s
 
@@ -32,7 +46,7 @@ class Grid():
     def check_grid(self):
         for i in range(self.size):
             for j in range(self.size):
-                if self.grid[i][j] not in range(0, 10):
+                if self.grid[i][j] not in range(0, self.size + 1):
                     print(f'Invalid number at position ({i}, {j})')
                     print(f'Number: {self.grid[i][j]}')
                     return False
@@ -59,10 +73,10 @@ class Grid():
                     self.skippable_numbers[pos].append(self.grid[row][i])
                 if self.grid[i][col] != 0:
                     self.skippable_numbers[pos].append(self.grid[i][col])
-            start_row = row - row % 3
-            start_col = col - col % 3
-            for i in range(3):
-                for j in range(3):
+            start_row = row - row % self.square_root_dim
+            start_col = col - col % self.square_root_dim
+            for i in range(self.square_root_dim):
+                for j in range(self.square_root_dim):
                     if self.grid[i + start_row][j + start_col] != 0:
                         self.skippable_numbers[pos].append(self.grid[i + start_row][j + start_col])
             self.skippable_numbers[pos] = list(set(self.skippable_numbers[pos]))
@@ -79,14 +93,50 @@ class Grid():
                 return False
 
         # Controllo quadrato
-        start_row = row - row % 3
-        start_col = col - col % 3
-        for i in range(3):
-            for j in range(3):
+        start_row = row - row % self.square_root_dim
+        start_col = col - col % self.square_root_dim
+        for i in range(self.square_root_dim):
+            for j in range(self.square_root_dim):
                 if self.grid[i + start_row][j + start_col] == num and (i + start_row, j + start_col) != (row, col):
                     return False
 
         return True
+    
+    def fill_grid(self, row=0, col=0):
+        if row == self.size - 1 and col == self.size:
+            return True
+        
+        if col == self.size:
+            row += 1
+            col = 0
+        
+        if self.grid[row][col] != 0:
+            return self.fill_grid(row, col + 1)
+        
+        nums = list(range(1, self.size + 1))
+        random.shuffle(nums)
+        for num in nums:
+            if self.is_valid(row, col, num):
+                self.grid[row][col] = num
+                if self.fill_grid(row, col + 1):
+                    return True
+                self.grid[row][col] = 0
+        return False
+    
+    def generate_grid(self):
+        self.grid = [[0 for _ in range(self.size)] for _ in range(self.size)]
+        self.fill_grid()
+    
+    def remove_numbers(self, n):
+        if n > self.size**2:
+            n = self.size**2
+        for _ in range(n):
+            row = random.randint(0, self.size - 1)
+            col = random.randint(0, self.size - 1)
+            while self.grid[row][col] == 0:
+                row = random.randint(0, self.size - 1)
+                col = random.randint(0, self.size - 1)
+            self.grid[row][col] = 0
 
     def init_solve(self):
         if self.check_grid():
@@ -114,7 +164,7 @@ class Grid():
             self.show_runtime(1, 1, 0)
             return True
         row, col = self.zeros[self.index]
-        for num in range(1, 10):
+        for num in range(1, self.size + 1):
             if num not in self.skippable_numbers[(row, col)]:
                 if self.is_valid(row, col, num):
                     self.grid[row][col] = num
@@ -148,8 +198,9 @@ def main():
         [0, 0, 0, 0, 8, 0, 0, 7, 9]
     ]
     
-    grid = Grid(grid_data)
-    grid.solve(show_kwargs={'steps': 100, 'sleep': 0.1})
+    grid = Grid.from_dim(4)
+    grid.remove_numbers(200)
+    grid.solve(show_kwargs={'steps': 10000, 'sleep': 0.1})
 
 if __name__ == '__main__':
     main()
